45272c17d1a470e104f06175248e3206dbf38261
aby <abigailtiet@gmail.com>
2022-03-11 13:13:36 -0500

    basic Go API with Gin exercise complete

--- main.go
+++ main.go
@@ -26,6 +26,11 @@ func main() {
 	router := gin.Default()
 	// Use the GET function to associate the GET HTTP method and /albums path with handler function
 	router.GET("/albums", getAlbums)
+	// Associate the /albums/:id path with the getAlbumByID function
+	// : symbol in path signifies item is a path parameter
+	router.GET("/albums/:id", getAlbumByID)
+	// Use POST function to associate the POST HTTP method and /albums path with handler function
+	router.POST("/albums", postAlbums)
 	// Run function to attach the router to an http.Server and start the server
 	router.Run("localhost:8080")
 }
@@ -36,4 +41,39 @@ func getAlbums(c *gin.Context) {
 
 	// Serialize the struct into JSON and add to response
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// postAlbums adds an album from JSON received in request body
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJson to bind received JSON to newAlbum
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add new album struct from JSON to albums slice
+	albums = append(albums, newAlbum)
+	// Add 201 status code to response and JSON representing album created
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID locates album whose ID value matches the id parameter sent
+// by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	// Use Context.param to retrieve the id path parameter from the URL
+	// When you map this handler to a path, you'll use a placeholder param in the path
+	id := c.Param("id")
+
+	// Loop over the list of albums looking for an album whose
+	// ID value matches the parameter
+	for _, a := range albums {
+		if a.ID == id {
+			// 200 status code and serialize album struct and return in response as JSON
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	// 404 not found
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Album not found"})
 }
